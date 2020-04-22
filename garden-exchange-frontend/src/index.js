861273152cd7916b7f4e146a3847b8f85e01f1dc
Brittany Javalera <brittjavalera@gmail.com>
2020-04-22 16:29:00 -0700

    created a setCurrentUser action

--- garden-exchange-frontend/src/index.js
+++ garden-exchange-frontend/src/index.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import App from './App';
-import { createStore } from 'redux';
-import { thunk } from 'redux-thunk';
+import {createStore, applyMiddleware, compose, combineReducers} from 'redux';
+//compose combines middlewares to combine them into one argument that gets passed to the store
+import thunk from 'redux-thunk';
 import { Provider } from 'react-redux';
-import currentUserReducer from './reducers/currentUserReducer'
+import currentUserReducer from './reducers/currentUserReducer.js';
+import { BrowserRouter as Router } from 'react-router-dom'
 
 
 const reducer = combineReducers({
