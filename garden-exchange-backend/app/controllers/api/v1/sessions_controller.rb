9e465783847e89769c46f87305c07cfa4412182c
Brittany Javalera <brittjavalera@gmail.com>
2020-04-22 18:12:51 -0700

    wrote out sessions#create method, updates login route, added current_user and logged_in helper methods to application controller

---
+++ garden-exchange-backend/app/controllers/api/v1/sessions_controller.rb
@@ -0,0 +1,13 @@
+class Api::V1::SessionsController < ApplicationController
+    def create
+       user = User.find_by(username: params[:session][:username])
+        if user && user.authenticate(params[:session][:password])
+            session[:user_id] = user.id
+            render json: UserSerializer.new(user).to_serialized_json
+        else
+            render json: {
+                error: "Invalid Username or Password"
+            }
+        end
+    end
+end
--- garden-exchange-backend/config/routes.rb
+++ garden-exchange-backend/config/routes.rb
@@ -1,10 +1,12 @@
 Rails.application.routes.draw do
-  namespace :api do 
-    namespace :v1 do
-      resources :users
-      resources :listings
-      resources :offers
+  post '/api/v1/login', to: 'api/v1/sessions#create'
+  
+    namespace :api do 
+      namespace :v1 do
+        resources :users
+        resources :listings
+        resources :offers
+      end
     end
-  end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
