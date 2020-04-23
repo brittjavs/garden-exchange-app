69491453518fd5ce358904f0cf26370ed51a49a1
Brittany Javalera <brittjavalera@gmail.com>
2020-04-22 19:34:10 -0700

    added a route for get_current_user. created get_current_user in sessions

--- garden-exchange-backend/app/controllers/api/v1/sessions_controller.rb
+++ garden-exchange-backend/app/controllers/api/v1/sessions_controller.rb
@@ -3,10 +3,21 @@ class Api::V1::SessionsController < ApplicationController
        user = User.find_by(username: params[:session][:username])
         if user && user.authenticate(params[:session][:password])
             session[:user_id] = user.id
-            render json: UserSerializer.new(user).to_serialized_json
+            byebug
+            render json: UserSerializer.new(current_user).to_serialized_json
         else
             render json: {
                 error: "Invalid Username or Password"
+            }
+        end
+    end
+
+    def get_current_user
+        if logged_in
+            render json: current_user
+        else
+            render json: {
+                error: "No one is logged in"
             }
         end
     end
