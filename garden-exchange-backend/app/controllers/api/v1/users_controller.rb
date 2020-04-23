de5b5523a479c0a8088a6996c2e8aa36ce6b716f
Brittany Javalera <brittjavalera@gmail.com>
2020-04-23 13:02:21 -0700

    created signup route. revised user create to assign session id to user id to create a session once signed up

--- garden-exchange-backend/app/controllers/api/v1/users_controller.rb
+++ garden-exchange-backend/app/controllers/api/v1/users_controller.rb
@@ -1,15 +1,14 @@
 class Api::V1::UsersController < ApplicationController
-    def index
-        users = User.all
-        render json: UserSerializer.new(users).to_serialized_json
-    end
-
     def create 
         user = User.new(user_params)
             if user.save
+                session[:user_id] = user.id
                 render json: UserSerializer.new(user).to_serialized_json
             else
-                render json: {error: 'Error creating user'}
+                resp = {
+                    error: user.errors.full_messages.to_sentence
+                }
+                render json: {errors: 'Error creating user'}
             end
     end
 
@@ -19,7 +18,9 @@ class Api::V1::UsersController < ApplicationController
     end
 
     private
+    
     def user_params
-        params.require(:user).permit(:username, :city, :state, :password)
+        params.require(:user).permit(:username, :password, :city, :state)
     end
+
 end
