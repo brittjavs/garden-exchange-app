e3bc3dab6f32a0b032759bc462653474e0e0f4ac
Brittany Javalera <brittjavalera@gmail.com>
2020-04-22 22:36:42 -0700

    added logged_in check for testing in listing, added serializer to the json rendered in get_current_user

--- garden-exchange-backend/app/controllers/api/v1/listings_controller.rb
+++ garden-exchange-backend/app/controllers/api/v1/listings_controller.rb
@@ -1,17 +1,26 @@
 class Api::V1::ListingsController < ApplicationController
    
     def index
-        listings = Listing.all
-        render json: ListingSerializer.new(listings).to_serialized_json
+        if logged_in
+            byebug
+            listings = Listing.all
+            render json: ListingSerializer.new(listings).to_serialized_json
+        else
+            render json: {error: "Please log in"}
+        end
     end
 
     def create
+        if logged_in
         listing = Listing.new(listing_params)
             if listing.save
                 render json: ListingSerializer.new(listing).to_serialized_json
             else
                 render json: {error: "Error creating listing"}
             end
+        else
+            render json: {error: "Please log in"}
+        end
     end
 
     def destroy
